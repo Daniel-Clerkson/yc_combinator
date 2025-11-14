933b2236859ece1f6c48a9aa9a6493d04d7c0366
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-11-14 18:18:23 +0000

    Second Commit

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -37,14 +37,14 @@ const StartupCard = async({ product }: { product: StartupTypecard }) => {
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${session?.id}`}>
+          <Link href={`/user/${author?.id}`}>
             <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h2 className="text-26-semibold line-clamp-1">{title}</h2>
           </Link>
         </div>
-        <Link href={`/user/${session?.id}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
             src={author?.image}
             alt="img"
