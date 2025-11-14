0f0cdd8952978df87065eb56269f461e79731d54
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-11-14 18:53:32 +0000

    Second Commit--2

--- auth.ts
+++ auth.ts
@@ -36,7 +36,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       if (account && profile) {
         const user = await client
           .withConfig({ useCdn: false })
-          .fetch(AUTHOR_BY_GITHUB_ID, {
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
             id: profile?.id,
           });
 
